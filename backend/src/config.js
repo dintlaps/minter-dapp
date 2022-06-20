97626130b3873f964db9a933e31ec2967c15371e
maxxpa <info@dint.com>
2022-06-20 19:29:54 -0400

    Update config.js

--- backend/src/config.js
+++ backend/src/config.js
@@ -67,7 +67,7 @@ const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the ro
 const ROYALTY_ADDRESS = "0xB408F352d2701Eb3D0DFC5843F45598fa3Cdd839"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = ["0xB408F352d2701Eb3D0DFC5843F45598fa3Cdd839"]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "0x108001A68A009DD4494DEA91d8961ce3a112368f"; // If you want to manually include it
